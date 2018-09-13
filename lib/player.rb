029368a048b781941cc17286de179ef55e9a445e
Daniel <dct.lau17@gmail.com>
2018-09-13 15:49:02 +0100

    Add show_card method to player class

--- lib/player.rb
+++ lib/player.rb
@@ -8,4 +8,10 @@ class Player
   def get_card(card)
     hand << card
   end
+
+  def show_hand
+    output = []
+    hand.each { |card| output << card.show_card }
+    output
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,9 +3,20 @@ require 'player'
 RSpec.describe Player do
   subject(:player) { described_class.new }
 
+  before(:each) do
+    @card1 = double(:card, show_card: 'Ace of Hearts')
+    @card2 = double(:card, show_card: 'Two of Hearts')
+    player.get_card(@card1)
+    player.get_card(@card2)
+  end
+
   it 'can add a card to the players hand' do
-    card = 'Ace of Hearts'
-    player.get_card(card)
-    expect(player.hand).to include card
+    expect(player.hand).to include @card1
+    expect(player.hand).to include @card2
+  end
+
+  it 'can show the cards in the players hand' do
+    expected_output = [@card1.show_card, @card2.show_card]
+    expect(player.show_hand).to eq expected_output
   end
 end
