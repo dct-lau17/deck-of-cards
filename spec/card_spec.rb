e024aa9894d32a0f44a7654801b1713308af9fef
Daniel <dct.lau17@gmail.com>
2018-09-13 13:10:50 +0100

    Add suit method to card class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,13 @@
 require 'card'
 
 RSpec.describe Card do
-  subject(:card) { described_class.new('Ace') }
+  subject(:card) { described_class.new('Ace', 'Heart') }
+
   it 'can record the value of a card' do
     expect(card.value).to eq 'Ace'
+  end
+
+  it 'can record the suit of a card' do
+    expect(card.suit).to eq 'Heart'
   end
 end
